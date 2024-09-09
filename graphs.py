b3ae5e59b4bfb5b3b0720192230833ffe45e4af7
Mustafa Al-Bayati <120540908+mustafa901@users.noreply.github.com>
2024-09-09 16:47:23 +0000

    Started with the graphs for Homework 1

---
+++ graphs.py
@@ -0,0 +1,15 @@
+# HomeWork 1 Graphing
+import pandas as pd
+import numpy as np
+
+sum1 = pd.read_csv("/workspaces/DISCO/sum1_runtimes.csv")
+sum2 = pd.read_csv("/workspaces/DISCO/sum2_runtimes.csv")
+
+print(sum1)
+print(sum2)
+
+# Printing out the graphs and recognizing the patterns
+# 1. Provide a plot of the results of your timing experiment.
+# 2. Based on the results of your timing experiment, which algorithm do you think is better? Why?
+# 3. Looking at the code for each of the algorithms, explain how each of them works.
+# 4. Based on a comparison of the code, why do you think one algorithm performs better than the other?
